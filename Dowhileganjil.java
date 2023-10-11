5dc311d383457e4d2dcc886c7cccbb72bf245f24
Alfian <144507303+malhandha@users.noreply.github.com>
2023-10-11 07:46:03 +0700

    Add files via upload

---
+++ Dowhileganjil.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dowhileganjil;
+import java.util.Scanner;
+/**
+ *
+ * @author malfi
+ */
+public class Dowhileganjil {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Menentukan bilangan ganjil menggunakan do while");
+        System.out.print("Masukkan Bilangan : ");
+        int x = s.nextInt();
+        System.out.println("Bilangan ganjilnya adalah : ");
+        int counter = 1;
+        while (x >= counter){
+            System.out.println(counter + " ");
+            counter += 2;
+        }
+    }
+}
